b0c65fbf3e751ffe3f99f8ee47da3e932aba50fa
Debajyoti Debnath <debajyotid2@hotmail.com>
2025-09-06 14:37:25 -0400

    Create separate modules and files

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
 CC=gcc
 CFLAGS=-Wall -Wextra -Wswitch-enum -std=c11 -pedantic
 LDFLAGS=
-BIN=main
+BIN=main evasm_to_vm
 
 all: $(BIN)
 
-main: main.c
+main: main.c vm.h
+	$(CC) $(CFLAGS) $^ -o $@ $(LDFLAGS)
+
+evasm_to_vm: evasm_to_vm.c vm.h
 	$(CC) $(CFLAGS) $^ -o $@ $(LDFLAGS)
 
 clean:
---
+++ evasm_to_vm.c
@@ -0,0 +1,19 @@
+#include "vm.h"
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+    if (argc < 3) {
+        fprintf(stderr, "ERROR: Usage: %s <input.evasm> <output.vm>\n",
+                argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    const char *input_file_path = argv[1];
+    const char *output_file_path = argv[2];
+
+    Inst       program[VM_PROGRAM_CAPACITY];
+    StringView source = slurp_file(input_file_path);
+    size_t     program_size =
+        vm_translate_source(source, program, VM_PROGRAM_CAPACITY);
+    vm_save_program_to_file(program, program_size, output_file_path);
+    return EXIT_SUCCESS;
+}
